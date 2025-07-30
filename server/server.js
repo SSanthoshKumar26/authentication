a91155ff7931e74bce39b1443255a10afc090ac7
Santhosh Kumar S <santolbs26@gmail.com>
2025-07-30 23:28:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,26 +11,30 @@ import userAuth from './middleware/userAuth.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
+// CORS setup using deployed frontend URL
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONTEND_URL, 
   credentials: true
 }));
 
+// Connect to DB
 connectDB();
 
-// Root endpoint to test if the API is working
+// Test route
 app.get('/', (req, res) => res.send('API Working Fine'));
 
-// Protected route for fetching user data
+// Protected user data route
 app.get('/api/auth/user', userAuth, getUserData);
 
-// Use the Auth and User routes
+// Routes
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
+// Start server
 app.listen(port, () => {
   console.log(`Server started on PORT: ${port}`);
 });
